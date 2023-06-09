3213e195ad32714dc3c123ee7cee1e4881ee4660
Allison Matias <105616885+allsnmts@users.noreply.github.com>
2023-06-09 19:46:26 +0800

    Update firebase.js

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -9,8 +9,8 @@ import {
 } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCyGN5LO-J23PUEV7yyvjOOwjAyahcimHM",
-  // apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  // apiKey: "AIzaSyCyGN5LO-J23PUEV7yyvjOOwjAyahcimHM",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
   authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
   projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
   storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
